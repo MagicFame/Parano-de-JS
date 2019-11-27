5b3653aa9aae2f7ccc31ba6006a2b5b4e7ef047d
Leomerran <leo.merran@gmail.com>
2019-11-27 16:25:05 +0100

    Revert "creation of objects"
    This reverts commit 8ffdaf659840b03d2c706bdf682a7dce5a6a8b84.

--- rest_api/api/models/userModel.js
+++ rest_api/api/models/userModel.js
@@ -8,13 +8,8 @@ var Schema = mongoose.Schema
 
 var UserSchema = new Schema({
   username: { type: String, required: true },
-  firstname: { type: String, required: true },
-  lastname: { type: String, required: true },
-  birthdate: { type: Date, required: true },
-  phone: { type: String, required: false },
   mail: { type: String, required: true },
   password: { type: String, required: true }
 })
 
 module.exports = mongoose.model('User', UserSchema)
-
